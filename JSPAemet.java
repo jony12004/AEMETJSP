01231a0e0a0c9fd60eacfdc2b5eb4b220e916639
jony12004 <alexsapu22@gmail.com>
2024-11-22 10:22:50 +0100

    Commit 10.22

--- JSPAemet.java
+++ JSPAemet.java
@@ -1,8 +1,29 @@
 public class JSPAemet {
-    public JSPAemet(){
+    public static void main(String[] args) {
+        JSPAemet.JSPGenerarRegistroTemp();
+        JSPAemet.JSPListarRegistroTemp();
 
     }
 
+    public static int CIUDAD = 8;
+    static String [] ciudad = {"Almeria","Granada","Cadiz","Malaga","Jaen", "Cordoba", "Huelva", "Sevilla"};
+    static int [] registroTemp = new int[8];
 
+    public static int JSPGenerarRegistroTemp(){
+        for (int i = 0; i < registroTemp.length; i){
 
+        }
+    }
+
+    public static void JSPListarRegistroTemp(){
+        System.out.println("------ LISTADO DE NOTAS --------------");
+        for (int j=0; j<JSPAemet.CIUDAD; j++){
+            System.out.print("             " + ciudad[j] + "  ");
+        }
+        for (int j=0; j<CIUDAD; j++){
+            for (int i=0; i<JSPRegCiudad.DIASEMANA; i++){
+                System.out.print("               " + JSPRegCiudad.temperatura[i][j]+ "        ");
+            }
+            System.out.println();
+        }    }
 }
--- JSPRegCiudad.java
+++ JSPRegCiudad.java
@@ -1,19 +1,33 @@
 import java.util.Scanner;
 
 public class JSPRegCiudad {
-    Scanner consola = new Scanner(System.in);
+    static Scanner consola = new Scanner(System.in);
 
-
-    String [] ciudad = {"Almeria","Granada","Cadiz","Malaga","Jaen", "Cordoba", "Huelva", "Sevilla"};
+    public static int DIASEMANA = 7;
+    public static int NTEMP = 3;
     String [] diasSemana = {"Lunes", "Martes","Miercoles", "Jueves", "Viernes", "Sabado", "Doming"};
-    int [][] notas = []
-    public void JSPregCiudad(String[] args) {
+    static int [][] temperatura = new int[JSPRegCiudad.DIASEMANA][JSPRegCiudad.NTEMP];
 
-    }
+    static int [] valores = new int{tmax, tmin};
 
-    public static int[] JSPgenerarTemperaturas(int temperatura){
-        temperatura = 0;
-
-    }
+//    public static void JSPGenerarRegistroTemp(){
+//        for (int i = 0; i < JSPRegCiudad.DIASEMANA){
+//            System.out.println(ciudad[i]);
+//            System.out.print("Valor maximo: " );
+//            int vmax = consola.nextInt();
+//        }
+//    }
+//
+//
+//    public static int[] JSPgenerarTemperaturas(int temp){
+//        int valor = 0;
+//        for (int i = 0; i < DIASEMANA; i++){
+//            System.out.println();
+//        }
+//
+//        temp = (int)Math.random() * valor;
+//        return valor;
+//
+//    }
 
 }
