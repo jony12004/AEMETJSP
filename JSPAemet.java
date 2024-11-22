0cc4edf1375cea56180c1a77ae2e69d690ee4c36
jony12004 <alexsapu22@gmail.com>
2024-11-22 09:36:19 +0100

    Commit 9.30

---
+++ JSPAemet.java
@@ -0,0 +1,8 @@
+public class JSPAemet {
+    public JSPAemet(){
+
+    }
+
+
+
+}
---
+++ JSPRegCiudad.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class JSPRegCiudad {
+    Scanner consola = new Scanner(System.in);
+
+
+    String [] ciudad = {"Almeria","Granada","Cadiz","Malaga","Jaen", "Cordoba", "Huelva", "Sevilla"};
+    String [] diasSemana = {"Lunes", "Martes","Miercoles", "Jueves", "Viernes", "Sabado", "Doming"};
+    int [][] notas = []
+    public void JSPregCiudad(String[] args) {
+
+    }
+
+    public static int[] JSPgenerarTemperaturas(int temperatura){
+        temperatura = 0;
+
+    }
+
+}
